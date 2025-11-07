87c27cd440974ed50b439269a7d42508bd201d13
ShantanuMtk <shantanumtk315@gmail.com>
2025-11-06 17:39:30 -0800

    Frontend code updated and ingress yaml included

--- spring-microservices-bookstore-demo/frontend/apollo-client.ts
+++ spring-microservices-bookstore-demo/frontend/apollo-client.ts
@@ -1,20 +1,11 @@
-// apollo-client.ts - Updated to use proxy (no env vars needed!)
+// Apollo client for connecting to GraphQL endpoints.
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-// ✅ Use relative URL - Next.js will proxy to API Gateway
-// No need for NEXT_PUBLIC_API_BASE anymore!
-const GRAPHQL_URL = '/graphql';
-
-console.log('[Apollo Client] GraphQL URL:', GRAPHQL_URL);
+const GRAPHQL_URL = '/api/graphql';
 
 const client = new ApolloClient({
   uri: GRAPHQL_URL,
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'cache-and-network',
-    },
-  },
 });
 
 export default client;
